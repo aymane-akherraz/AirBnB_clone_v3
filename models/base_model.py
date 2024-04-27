79fca2bcae7f84a94273c8fd87b01fd8ae724d7e
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-04-27 19:43:50 +0100

    Modified user

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,6 @@ Contains class BaseModel
 
 from datetime import datetime
 import models
-from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
@@ -68,7 +67,7 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if getenv("HBNB_TYPE_STORAGE") == "db" and "password" in new_dict:
+        if models.storage_t == "db" and "password" in new_dict:
             del new_dict['password']
         return new_dict
 
