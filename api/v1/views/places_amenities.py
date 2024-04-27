3514f747bde076daa21cd49397c5ee2b06f1439a
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-04-27 19:24:16 +0100

    Added some files

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Place_amenities Module """
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+
+
+@app_views.route('/places/<place_id>/amenities', methods=['GET'])
+def get_place_amenities(place_id):
+    """ Retrieves the list of all Amenity objects of a Place """
+
+    place = storage.get("Place", place_id)
+    if place is None:
+        abort(404)
+    return jsonify([amenity.to_dict() for amenity in place.amenities])
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'])
+def delete_amenity(place_id, amenity_id):
+    """ Deletes a Amenity object to a Place """
+
+    place = storage.get("Place", place_id)
+    if place is None:
+        abort(404)
+
+    amenity = storage.get("Amenity", amenity_id)
+    if amenity is None:
+        abort(404)
+
+    if amenity not in place.amenities:
+        abort(404)
+    amenity.delete()
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['POST'])
+def link_Amenity(place_id, amenity_id):
+    """ Link a Amenity object to a Place """
+
+    place = storage.get("Place", place_id)
+    if place is None:
+        abort(404)
+
+    amenity = storage.get("Amenity", amenity_id)
+    if amenity is None:
+        abort(404)
+
+    if amenity in place.amenities:
+        return jsonify(amenity.to_dict())
+    return jsonify(amenity.to_dict()), 201
