a8e6eaf4b1f0f3c28b63df12b01b0b8549dbc0ee
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-04-28 16:54:41 +0100

    Modified user.py

--- models/user.py
+++ models/user.py
@@ -28,9 +28,9 @@ class User(BaseModel, Base):
     def __setattr__(self, name, value):
         """ Hashes the password """
         if name == 'password':
-            super().__setattr__(name, md5(value.encode()).hexdigest())
+            super(User, self).__setattr__(name, md5(value.encode()).hexdigest())
         else:
-            super().__setattr__(name, value)
+            super(User, self).__setattr__(name, value)
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
