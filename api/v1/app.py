1aa648423170089362ba1728b2c88b23adcd4fb6
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-04-28 16:38:31 +0100

    Modified app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,7 +18,11 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 CORS(app, origins="0.0.0.0")
+
+api_host = getenv("HBNB_API_HOST", "0.0.0.0")
+api_port = getenv("HBNB_API_PORT", 5000)
 
 
 @app.teardown_appcontext
@@ -34,6 +38,4 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", 5000)
-    app.run(host=host, port=int(port), threaded=True)
+    app.run(host=api_host, port=int(api_port), threaded=True)
